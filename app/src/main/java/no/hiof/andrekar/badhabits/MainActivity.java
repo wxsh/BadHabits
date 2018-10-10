2b288ecebdcf057f787968c53305d34a9e82e79a
Dennis <denniss@hiof.no>
2018-10-10 14:40:18 +0200

    View habits

--- app/src/main/java/no/hiof/andrekar/badhabits/MainActivity.java
+++ app/src/main/java/no/hiof/andrekar/badhabits/MainActivity.java
@@ -5,14 +5,26 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.Date;
+
+import model.DateHabit;
+import model.EconomicHabit;
+import model.Habit;
+
 public class MainActivity extends AppCompatActivity {
 
     //TODO: RecyclerView
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +32,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //temp: adding some habits - will be replaced with stored files
+        Habit sodaHabit = new DateHabit("Soda", "Stop drinking soda", new Date(),10);
+        Habit gumHabit  = new EconomicHabit("Gum", "stop with gum",new Date(),"kr",10,100,10);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab_addHabit);
         fab.setOnClickListener(new View.OnClickListener() {
